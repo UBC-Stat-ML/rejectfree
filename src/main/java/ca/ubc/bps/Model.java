17a66a882830b22dc1995e064b44e8a4d43b2bb5
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-03-27 22:50:27 +0100

    Upgrade some inits stuff

--- src/main/java/ca/ubc/bps/Model.java
+++ src/main/java/ca/ubc/bps/Model.java
@@ -3,8 +3,9 @@ package ca.ubc.bps;
 import blang.inits.Implementations;
 import ca.ubc.bps.BPSFactory.ModelBuildingContext;
 import ca.ubc.bps.models.FixedPrecisionNormalModel;
+import ca.ubc.bps.models.GeneralizedNormalModel;
 
-@Implementations({FixedPrecisionNormalModel.class})
+@Implementations({FixedPrecisionNormalModel.class, GeneralizedNormalModel.class})
 public interface Model
 {
   public void setup(ModelBuildingContext context, boolean initializeStatesFromStationary);
