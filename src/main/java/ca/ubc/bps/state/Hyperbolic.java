542b5e1797e3c0ab4fa3ee589d57f7c39484c545
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-04-23 14:24:56 +0100

    Add basic processing support for non-linear dynamics

--- src/main/java/ca/ubc/bps/state/Hyperbolic.java
+++ src/main/java/ca/ubc/bps/state/Hyperbolic.java
@@ -5,9 +5,19 @@ public class Hyperbolic implements Dynamics
   @Override
   public void extrapolateInPlace(double deltaTime, MutableDouble _position, MutableDouble velocity)
   {
-    TransformedMutableDouble position = (TransformedMutableDouble) _position;
-    double updatedBd = position.getBounded() + deltaTime * velocity.get();
-    position.setBoundedCoordinate(updatedBd);
+    if (_position instanceof TransformedMutableDouble)
+    {
+      TransformedMutableDouble position = (TransformedMutableDouble) _position;
+      double updatedBd = position.getBounded() + deltaTime * velocity.get();
+      position.setBoundedCoordinate(updatedBd);
+    }
+    else
+    {
+      // this will still be OK for interpolation (e.g. in processors) but not extrapolation, because of 
+      // cases where going out of bound, in which case we need TransformedMutableDouble implementation and 
+      // the above code
+      _position.set(toUnbCoord(toBdCoord(_position.get()) + deltaTime * velocity.get())); 
+    }
   } 
     
   public static double toBdCoord(double unb)
